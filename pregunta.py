93e568b49e833f40280c6a28bee92d78d6b11eb8
Diegoeraso1239 <53222685+Diegoeraso1239@users.noreply.github.com>
2022-10-24 10:23:06 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -84,4 +84,5 @@ def ingest_data():
   df.cantidad_de_palabras_clave = df.cantidad_de_palabras_clave.astype(int)
   df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.astype(float)
   df.cluster = df.cluster.astype(int) 
+  df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace('.','')
   return df
